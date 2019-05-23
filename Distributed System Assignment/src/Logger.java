6263da597ce16ab0d6d93dda99d8c4819ab05119
Scott Greene <scott-greene@hotmail.com>
2019-05-22 17:29:35 -0700

    Modified logger file d

--- Distributed System Assignment/src/Logger.java
+++ Distributed System Assignment/src/Logger.java
@@ -1,20 +1,9 @@
 public class Logger {
 
-    public static void main(String[] args){
-
-
-
-
-    }
-
     Logger ()
     {
 
     }
-
-    public void connectToLogFile(String logName)
-    {
-
-    }
+    
 
 }
